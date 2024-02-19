edf199f8a1bf305c3e9a775ba7ab983ab5ddb2f1
AlexanderGal86 <71893409+AlexanderGal86@users.noreply.github.com>
2024-02-19 19:04:03 +0300

    Add files via upload

---
+++ KES-collect.ps1
@@ -0,0 +1,8 @@
+﻿$cred=Get-Credential
+
+$list= ((Import-Csv -Path $env:HOMEPATH\Documents\KES\hosts.csv).Name)
+
+Invoke-Command -ComputerName $list -Credential $cred -ScriptBlock {
+                                                        Get-WmiObject -Class Win32_Product | where -Property Name -like "*Kaspersky*" | select Name, Version | sort PSComputerName 
+                                                        } | Export-Csv -Path $env:HOMEPATH\Documents\KES\hosts-out.csv -NoTypeInformation -Encoding UTF8
+
