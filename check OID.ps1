013ca7b9a7b31a36ea1303a40f5fe98a424021f3
AlexanderGal86 <71893409+AlexanderGal86@users.noreply.github.com>
2024-02-19 19:06:30 +0300

    Add files via upload

---
+++ check OID.ps1
@@ -0,0 +1,7 @@
+﻿$IP = "192.168.130.26"
+$SNMP = New-Object -ComObject olePrn.OleSNMP
+$SNMP.Open($IP, "public")
+$model = $SNMP.Get(1.3.6.1.2.1.43.10.2.1.4.1.1)
+$SNMP.Close()
+Write-host "$model"
+
