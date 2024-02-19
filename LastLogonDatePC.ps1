cf3bc508114890d6abd3eae533abd32511e8ecfc
AlexanderGal86 <71893409+AlexanderGal86@users.noreply.github.com>
2024-02-19 19:10:08 +0300

    Add files via upload

---
+++ LastLogonDatePC.ps1
@@ -0,0 +1,29 @@
+﻿$users = Get-ADComputer -Filter {OperatingSystem -notLike '*SERVER*' } -Properties LastLogonDate | Sort Name 
+
+$usersWithFormattedTimestamp = foreach ($user in $users) {
+
+    $formattedTimestamp = if ($user.LastLogonTimestamp) {
+
+        [DateTime]::FromFileTime($user.LastLogonTimestamp).ToString("yyyy-MM-dd HH:mm:ss")
+
+    } else {
+
+        ""
+
+    }
+
+    
+
+    [PSCustomObject]@{
+
+        Name              = $user.Name
+
+        WhenCreated       = $user.WhenCreated
+
+        LastLogonTimestamp = $formattedTimestamp
+
+    }
+
+}
+
+$usersWithFormattedTimestamp | Export-Csv $env:HOMEPATH\Desktop\adcomputers-last-logon2.csv -NoTypeInformation
